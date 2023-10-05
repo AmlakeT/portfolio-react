5afd860f97a215ca42f43e8a858b6ed22d282020
AmalkeT <amxmulu123@gmail.com>
2023-10-05 14:06:38 +0300

    starter

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className=' text-blue-600 text-center mt-32 font font-bold text-2xl '>
+     <h1>Hello, this a react starter:)</h1>
+    </div>
+  )
+}
+
+export default App
